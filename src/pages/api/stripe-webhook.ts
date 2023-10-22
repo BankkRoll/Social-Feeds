d38e8343386c03b4f5efbfd3b6ee2be97891f0ac
Bankk <106103625+BankkRoll@users.noreply.github.com>
2023-10-22 12:17:12 -0400

    revert

--- src/pages/api/stripe-webhook.ts
+++ src/pages/api/stripe-webhook.ts
@@ -3,28 +3,6 @@ import Stripe from "stripe";
 import { doc, setDoc } from "firebase/firestore";
 import { db } from "../../../firebaseClient";
 
-const buffer = (req: NextApiRequest) => {
-  return new Promise<Buffer>((resolve, reject) => {
-    const chunks: Buffer[] = [];
-
-    req.on('data', (chunk: Buffer) => {
-      chunks.push(chunk);
-    });
-
-    req.on('end', () => {
-      resolve(Buffer.concat(chunks));
-    });
-
-    req.on('error', reject);
-  });
-};
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2023-10-16",
 });
@@ -34,15 +12,12 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
-    const rawBody = await buffer(req);
-
     const sig = req.headers["stripe-signature"]!;
-
     let event: Stripe.Event;
 
     try {
       event = stripe.webhooks.constructEvent(
-        rawBody.toString(),
+        req.body,
         sig,
         process.env.STRIPE_WEBHOOK_SECRET!
       );
@@ -61,23 +36,26 @@ export default async function handler(
           await setDoc(userRef, { proUser: true }, { merge: true });
         }
         break;
+
       case "customer.subscription.deleted":
         if (userAddress) {
           await setDoc(userRef, { proUser: false }, { merge: true });
         }
         break;
+
       case "customer.subscription.paused":
         if (userAddress) {
           await setDoc(userRef, { proUser: false }, { merge: true });
         }
         break;
+
       case "customer.subscription.resumed":
         if (userAddress) {
           await setDoc(userRef, { proUser: true }, { merge: true });
         }
         break;
+
       default:
-        // Unhandled event type
     }
 
     res.json({ received: true });
