144ef40ba2983beb436ad97a18befa708196380c
Bankk <106103625+BankkRoll@users.noreply.github.com>
2023-10-23 12:48:00 -0400

    webhook works local but wont work on vercel
    webhook works local but wont work on vercel trying for the 69th time

--- src/pages/api/stripe-webhook.ts
+++ src/pages/api/stripe-webhook.ts
@@ -1,8 +1,12 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import { buffer } from 'micro';
+import { NextApiRequest, NextApiResponse } from 'next';
 import Stripe from 'stripe';
 import { doc, setDoc } from 'firebase/firestore';
 import { db } from '../../../firebaseClient';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2023-10-16",
+});
 
 export const config = {
   api: {
@@ -10,14 +14,12 @@ export const config = {
   },
 };
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16',
-});
+const updateUserStatus = async (userAddress: string, proUser: boolean, subscriptionStatus: string) => {
+  const userRef = doc(db, "users", userAddress);
+  await setDoc(userRef, { proUser, subscriptionStatus }, { merge: true });
+};
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const buf = await buffer(req);
     const sig = req.headers['stripe-signature']!;
@@ -29,38 +31,35 @@ export default async function handler(
       return res.status(400).send(`Webhook Error: ${(err as Error).message}`);
     }
 
-    const subscription = event.data.object as Stripe.Subscription;
-    const userAddress = subscription.metadata.userAddress;
-    const userRef = doc(db, 'users', userAddress);
+    const data: Stripe.Event.Data = event.data;
+    const eventType: string = event.type;
+    const subscription: Stripe.Subscription = data.object as Stripe.Subscription;
+    const userAddress: string = subscription.metadata?.userAddress || "";
 
-    switch (event.type) {
-      case 'customer.subscription.created':
-      case 'customer.subscription.updated':
-        if (userAddress) {
-          await setDoc(userRef, { proUser: true }, { merge: true });
-        }
+    switch (eventType) {
+      case "customer.subscription.created":
+        await updateUserStatus(userAddress, true, "active");
         break;
-      case 'customer.subscription.deleted':
-        if (userAddress) {
-          await setDoc(userRef, { proUser: false }, { merge: true });
-        }
+      case "customer.subscription.deleted":
+        await updateUserStatus(userAddress, false, "deleted");
         break;
-      case 'customer.subscription.paused':
-        if (userAddress) {
-          await setDoc(userRef, { proUser: false }, { merge: true });
-        }
+      case "customer.subscription.paused":
+        await updateUserStatus(userAddress, false, "paused");
         break;
-      case 'customer.subscription.resumed':
-        if (userAddress) {
-          await setDoc(userRef, { proUser: true }, { merge: true });
-        }
+      case "customer.subscription.resumed":
+        await updateUserStatus(userAddress, true, "active");
+        break;
+      case "customer.subscription.updated":
+        const newStatus = subscription.status;
+        await updateUserStatus(userAddress, newStatus === "active", newStatus);
         break;
       default:
+        return res.status(400).end();
     }
 
     res.json({ received: true });
   } else {
-    res.setHeader('Allow', 'POST');
-    res.status(405).end('Method Not Allowed');
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
   }
 }
