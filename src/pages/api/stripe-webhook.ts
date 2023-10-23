f21e1845ce6b68113ee4a0811728bf47b9c1c807
Bankk <106103625+BankkRoll@users.noreply.github.com>
2023-10-23 12:54:20 -0400

    maybe it works maybe it dont. vercel dont like me

--- src/pages/api/stripe-webhook.ts
+++ src/pages/api/stripe-webhook.ts
@@ -1,8 +1,7 @@
-import { buffer } from 'micro';
-import { NextApiRequest, NextApiResponse } from 'next';
-import Stripe from 'stripe';
-import { doc, setDoc } from 'firebase/firestore';
-import { db } from '../../../firebaseClient';
+import { NextApiRequest, NextApiResponse } from "next";
+import Stripe from "stripe";
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../../../firebaseClient";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2023-10-16",
@@ -14,47 +13,55 @@ export const config = {
   },
 };
 
-const updateUserStatus = async (userAddress: string, proUser: boolean, subscriptionStatus: string) => {
-  const userRef = doc(db, "users", userAddress);
-  await setDoc(userRef, { proUser, subscriptionStatus }, { merge: true });
-};
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    const buf = await buffer(req);
-    const sig = req.headers['stripe-signature']!;
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const sig = req.headers["stripe-signature"]!;
     let event: Stripe.Event;
 
     try {
-      event = stripe.webhooks.constructEvent(buf.toString(), sig, process.env.STRIPE_WEBHOOK_SECRET!);
+      event = stripe.webhooks.constructEvent(
+        req.body,
+        sig,
+        process.env.STRIPE_WEBHOOK_SECRET!
+      );
     } catch (err) {
       return res.status(400).send(`Webhook Error: ${(err as Error).message}`);
     }
 
-    const data: Stripe.Event.Data = event.data;
-    const eventType: string = event.type;
-    const subscription: Stripe.Subscription = data.object as Stripe.Subscription;
-    const userAddress: string = subscription.metadata?.userAddress || "";
+    const subscription = event.data.object as Stripe.Subscription;
+    const userAddress = subscription.metadata.userAddress;
+    const userRef = doc(db, "users", userAddress);
 
-    switch (eventType) {
+    switch (event.type) {
       case "customer.subscription.created":
-        await updateUserStatus(userAddress, true, "active");
-        break;
-      case "customer.subscription.deleted":
-        await updateUserStatus(userAddress, false, "deleted");
-        break;
-      case "customer.subscription.paused":
-        await updateUserStatus(userAddress, false, "paused");
-        break;
-      case "customer.subscription.resumed":
-        await updateUserStatus(userAddress, true, "active");
-        break;
       case "customer.subscription.updated":
-        const newStatus = subscription.status;
-        await updateUserStatus(userAddress, newStatus === "active", newStatus);
+        if (userAddress) {
+          await setDoc(userRef, { proUser: true }, { merge: true });
+        }
         break;
+
+      case "customer.subscription.deleted":
+        if (userAddress) {
+          await setDoc(userRef, { proUser: false }, { merge: true });
+        }
+        break;
+
+      case "customer.subscription.paused":
+        if (userAddress) {
+          await setDoc(userRef, { proUser: false }, { merge: true });
+        }
+        break;
+
+      case "customer.subscription.resumed":
+        if (userAddress) {
+          await setDoc(userRef, { proUser: true }, { merge: true });
+        }
+        break;
+
       default:
-        return res.status(400).end();
     }
 
     res.json({ received: true });
