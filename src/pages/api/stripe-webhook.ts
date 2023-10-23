43d42e90cf63fecfbb5743c0fb504665631bc36b
Bankk <106103625+BankkRoll@users.noreply.github.com>
2023-10-23 10:52:03 -0400

    .

--- src/pages/api/stripe-webhook.ts
+++ src/pages/api/stripe-webhook.ts
@@ -1,59 +1,66 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import Stripe from "stripe";
-import { doc, setDoc, collection, where, getDocs, addDoc, updateDoc, query } from "firebase/firestore";
-import { db } from "../../../firebaseClient";
-import { buffer } from "micro";
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-10-16",
-});
-
-const webhookSecret: string = process.env.STRIPE_WEBHOOK_SECRET!;
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "POST") {
-    const reqBuffer = await buffer(req);
-    const payload = reqBuffer.toString();
-    const signature = req.headers["stripe-signature"]!;
-
-    let event: Stripe.Event;
-
-    try {
-      event = stripe.webhooks.constructEvent(payload, signature, webhookSecret);
-    } catch (err: any) {
-      return res.status(400).send(`Webhook Error: ${err.message}`);
-    }
-
-    const userRef = collection(db, "users");
-
-    switch (event.type) {
-      case "customer.subscription.created":
-      case "customer.subscription.updated":
-      case "customer.subscription.deleted":
-      case "customer.subscription.paused":
-      case "customer.subscription.resumed":
-        const subscription = event.data.object as Stripe.Subscription;
-        const userAddress = subscription.metadata.userAddress;
-        const snapshot = await getDocs(query(userRef, where("userAddress", "==", userAddress)));
-        
-        if (!snapshot.empty) {
-          const docRef = snapshot.docs[0].ref;
-          const proStatus = ["customer.subscription.created", "customer.subscription.updated", "customer.subscription.resumed"].includes(event.type);
-          await updateDoc(docRef, { proUser: proStatus });
-        }
-        break;
-    }
-
-    res.status(200).json({ received: true });
-  } else {
-    res.setHeader("Allow", "POST");
-    res.status(405).end("Method Not Allowed");
-  }
-}
+import { NextApiRequest, NextApiResponse } from 'next';
+import { buffer } from 'micro';
+import Stripe from 'stripe';
+import { doc, setDoc } from 'firebase/firestore';
+import { db } from '../../../firebaseClient';
 
 export const config = {
   api: {
     bodyParser: false,
-    externalResolver: true,
   },
 };
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: '2023-10-16',
+});
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'POST') {
+    const buf = await buffer(req);
+    const sig = req.headers['stripe-signature']!;
+    let event: Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(buf.toString(), sig, process.env.STRIPE_WEBHOOK_SECRET!);
+    } catch (err) {
+      return res.status(400).send(`Webhook Error: ${(err as Error).message}`);
+    }
+
+    const subscription = event.data.object as Stripe.Subscription;
+    const userAddress = subscription.metadata.userAddress;
+    const userRef = doc(db, 'users', userAddress);
+
+    switch (event.type) {
+      case 'customer.subscription.created':
+      case 'customer.subscription.updated':
+        if (userAddress) {
+          await setDoc(userRef, { proUser: true }, { merge: true });
+        }
+        break;
+      case 'customer.subscription.deleted':
+        if (userAddress) {
+          await setDoc(userRef, { proUser: false }, { merge: true });
+        }
+        break;
+      case 'customer.subscription.paused':
+        if (userAddress) {
+          await setDoc(userRef, { proUser: false }, { merge: true });
+        }
+        break;
+      case 'customer.subscription.resumed':
+        if (userAddress) {
+          await setDoc(userRef, { proUser: true }, { merge: true });
+        }
+        break;
+      default:
+    }
+
+    res.json({ received: true });
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
+}
